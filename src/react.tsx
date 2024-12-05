cec5da120e98b935e59dbb0096d5d3f6dcd7e371
Will Stone <654103+will-stone@users.noreply.github.com>
2024-12-05 21:09:16 +0000

    ~ Provide initial state to server snapshot of React store

--- src/react.tsx
+++ src/react.tsx
@@ -5,12 +5,14 @@ import type { TwoAndEight } from './2n8.js'
 export function createStore<Store extends TwoAndEight>(
   store: Store,
 ): <StoreField>(
-  selector: (state: Omit<Store, '__getSnapshot' | '__subscribe'>) => StoreField,
+  selector: (state: Omit<Store, '$react' | '$subscribe'>) => StoreField,
 ) => StoreField {
+  // eslint-disable-next-line unicorn/prefer-structured-clone
+  const initialState = JSON.parse(JSON.stringify(store))
   return (selector) =>
     useSyncExternalStore(
-      store.__subscribe,
-      () => selector(store.__getSnapshot()),
-      () => selector(store.__getSnapshot()),
+      store.$subscribe,
+      () => selector(store),
+      () => selector(initialState),
     )
 }
