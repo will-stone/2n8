40d2a9a386a3beef7949507efb9d060b2359dcee
Will Stone <654103+will-stone@users.noreply.github.com>
2024-12-05 08:52:44 +0000

    Add favicon to website

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,55 @@ export default defineConfig({
   integrations: [
     react(),
     starlight({
+      favicon: '/favicon.svg',
+      head: [
+        {
+          attrs: {
+            href: '/favicon-96x96.png',
+            rel: 'icon',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          tag: 'link',
+        },
+        {
+          attrs: {
+            href: '/favicon.svg',
+            rel: 'icon',
+            type: 'image/svg+xml',
+          },
+          tag: 'link',
+        },
+        {
+          attrs: {
+            href: '/favicon.ico',
+            rel: 'shortcut icon',
+          },
+          tag: 'link',
+        },
+        {
+          attrs: {
+            href: '/apple-touch-icon.png',
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+          },
+          tag: 'link',
+        },
+        {
+          attrs: {
+            content: '2n8',
+            name: 'apple-mobile-web-app-title',
+          },
+          tag: 'meta',
+        },
+        {
+          attrs: {
+            href: '/site.webmanifest',
+            rel: 'manifest',
+          },
+          tag: 'link',
+        },
+      ],
       logo: {
         src: './website/assets/logo.png',
       },
