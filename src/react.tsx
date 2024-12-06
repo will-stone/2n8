69163c24d620f9069a52681c8aed69b7e812eb56
Will Stone <654103+will-stone@users.noreply.github.com>
2024-12-06 18:47:00 +0000

    ~ Remove API methods from React hook

--- src/react.tsx
+++ src/react.tsx
@@ -5,7 +5,12 @@ import type { TwoAndEight } from './2n8.js'
 export function createStore<Store extends TwoAndEight>(
   store: Store,
 ): <StoreField>(
-  selector: (state: Omit<Store, '$react' | '$subscribe'>) => StoreField,
+  selector: (
+    state: Omit<
+      Store,
+      '$reset' | '$subscribe' | '$getState' | '$getInitialState'
+    >,
+  ) => StoreField,
 ) => StoreField {
   // eslint-disable-next-line unicorn/prefer-structured-clone
   const initialState = JSON.parse(JSON.stringify(store))
