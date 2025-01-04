723c785a4bec3bc6b1ef9a16a8662f9350a03388
Will Stone <654103+will-stone@users.noreply.github.com>
2025-01-04 19:53:47 +0000

    Not sure if moving setup outside bench tests is accurate

--- src/2n8.bench.ts
+++ src/2n8.bench.ts
@@ -1,24 +1,25 @@
+import { effect, signal } from 'alien-signals'
 import { autorun, makeAutoObservable } from 'mobx'
 import { bench, describe } from 'vitest'
 
 import { createStore, TwoAndEight } from './2n8.js'
 
 describe('2n8', () => {
-  class Store extends TwoAndEight {
-    count = 0
-
-    increaseCount() {
-      this.count = this.count + 1
-    }
-
-    resetAll() {
-      this.$reset()
-    }
-  }
-
-  const store = createStore(new Store())
-
   bench('simple count', async () => {
+    class Store extends TwoAndEight {
+      count = 0
+
+      increaseCount() {
+        this.count = this.count + 1
+      }
+
+      resetAll() {
+        this.$reset()
+      }
+    }
+
+    const store = createStore(new Store())
+
     const subscriptionComplete = new Promise<void>((resolve) => {
       const unsubscribe = store.subscribe(
         () => {
@@ -39,25 +40,25 @@ describe('2n8', () => {
 })
 
 describe('mobx', () => {
-  class Store {
-    count = 0
-
-    constructor() {
-      makeAutoObservable(this)
-    }
-
-    increaseCount() {
-      this.count = this.count + 1
-    }
-
-    resetAll() {
-      this.count = 0
-    }
-  }
-
-  const store = new Store()
-
   bench('simple count', async () => {
+    class Store {
+      count = 0
+
+      constructor() {
+        makeAutoObservable(this)
+      }
+
+      increaseCount() {
+        this.count = this.count + 1
+      }
+
+      resetAll() {
+        this.count = 0
+      }
+    }
+
+    const store = new Store()
+
     const subscriptionComplete = new Promise<void>((resolve) => {
       autorun(() => {
         if (store.count === 1) {
@@ -70,5 +71,23 @@ describe('mobx', () => {
 
     await subscriptionComplete
     store.resetAll()
+  })
+})
+
+describe('alien-signals', () => {
+  bench('simple count', async () => {
+    const count = signal(1)
+
+    const subscriptionComplete = new Promise<void>((resolve) => {
+      effect(() => {
+        if (count.get() === 1) {
+          resolve()
+        }
+      })
+    })
+
+    count.set(1)
+    await subscriptionComplete
+    count.set(0)
   })
 })
