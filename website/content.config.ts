e357f211a66132c4893efce221f3dee5ddaf22f5
Will Stone <654103+will-stone@users.noreply.github.com>
2025-01-20 13:46:56 +0000

    Add site banner

--- website/content.config.ts
+++ website/content.config.ts
@@ -1,7 +1,17 @@
 import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
-import { defineCollection } from 'astro:content'
+import { defineCollection, z } from 'astro:content'
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        banner: z.object({ content: z.string() }).default({
+          content:
+            'This library is still in its infancy. Please <a href="https://github.com/will-stone/2n8/issues" target="_blank">log any issues</a> you may find.',
+        }),
+      }),
+    }),
+  }),
 }
