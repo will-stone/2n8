e94c9f25a8de3f89f0e13f1441ea3ecbb333ab83
Will Stone <654103+will-stone@users.noreply.github.com>
2025-02-18 18:57:20 +0000

    Remove site banner

--- website/content.config.ts
+++ website/content.config.ts
@@ -1,17 +1,10 @@
 import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
 
 export const collections = {
   docs: defineCollection({
     loader: docsLoader(),
-    schema: docsSchema({
-      extend: z.object({
-        banner: z.object({ content: z.string() }).default({
-          content:
-            'This library is still in its infancy. Please <a href="https://github.com/will-stone/2n8/issues" target="_blank">log any issues</a> you may find.',
-        }),
-      }),
-    }),
+    schema: docsSchema(),
   }),
 }
